d2b2f80c99bf75380a2d9c3295b369081c9d1e17
shwsun <shwsun@go-merry.com>
2022-02-18 16:59:42 +0900

    pg to maria

--- docker/hive-single/install-hive-single.sh
+++ docker/hive-single/install-hive-single.sh
@@ -77,7 +77,7 @@ cp $HADOOP_HOME/share/hadoop/hdfs/lib/guava-27.0-jre.jar $HIVE_HOME/lib
 # 6. init schema 
 echo "---- Ready to init schama ----"
 ## 리모트 방식 
-$HIVE_HOME/bin/schematool -dbType postgres -initSchema -userName postgres -passWord 1234
+$HIVE_HOME/bin/schematool -dbType mysql -initSchema -userName hive -passWord hive
 #$HIVE_HOME/bin/schematool -dbType postgres -initSchema 
 # 7. hive 서버 실행  
 $HIVE_HOME/bin/hiveserver2
