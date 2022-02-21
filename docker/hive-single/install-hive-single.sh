252083dc68152eed3d4e1fc16badc61cdc9d71b9
shwsun <shwsun@go-merry.com>
2022-02-21 17:22:00 +0900

    hive-compose

--- docker/hive-single/install-hive-single.sh
+++ docker/hive-single/install-hive-single.sh
@@ -1,13 +1,13 @@
 # install-hadoop-single.sh 
 echo "---- Hive installation started. ----"
-export HIVE_VER=3.2.1 # 2.3.9
+export HIVE_VER=3.1.2 # 2.3.9
 wget https://dlcdn.apache.org/hive/hive-${HIVE_VER}/apache-hive-${HIVE_VER}-bin.tar.gz
 mkdir /hive
 tar -xvf apache-hive-${HIVE_VER}-bin.tar.gz -C /hive
 
 cat <<EOF |tee -a ~/.bashrc
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export HADOOP_HOME=/hadoop/hadoop-3.2.2
+export HADOOP_HOME=/hadoop/hadoop-3.3.1
 export HIVE_HOME=/hive/apache-hive-${HIVE_VER}-bin
 export PATH=\$PATH:\$JAVA_HOME/bin:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin:\$HIVE_HOME/bin
 EOF
