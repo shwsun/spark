b2305a53338e25703e0e6e88d1b4269f7268bd4e
shwsun <shwsun@naver.com>
2022-03-03 08:49:30 +0000

    mysql hive db creation

--- docker/hdfs-cluster/mysql/install-mysql.sh
+++ docker/hdfs-cluster/mysql/install-mysql.sh
@@ -15,10 +15,10 @@ set global validate_password_length=4;
 CREATE USER 'hive'@'%' IDENTIFIED BY 'hive';
 
 CREATE DATABASE metastore;
-GRANT ALL privileges on *.* to 'hive'@'%' with GRANT option;
+GRANT ALL privileges on metastore.* to 'hive'@'%' with GRANT option;
 flush privileges;
 EOF
-#mysql -u root -p"\n" < /install-files/metastore-creation.sh
+mysql -u root -p"\n" < /install-files/metastore-creation.sh
 
 #### create hue db ####
 cat <<EOF |tee /install-files/hue_db-creation.sh
