e3592d0ed004c69bc26942ced438b8a4a135310f
shwsun <shwsun@naver.com>
2022-03-15 16:23:42 +0900

    cluster size down 2

--- docker/hdfs-cluster/cluster-shell/sync_key.sh
+++ docker/hdfs-cluster/cluster-shell/sync_key.sh
@@ -11,20 +11,20 @@ docker cp dn01:/root/.ssh/id_rsa.pub ./tmp-share/key_pub
 cat ./tmp-share/key_pub >> ./tmp-share/id_rsa.pub
 docker cp dn02:/root/.ssh/id_rsa.pub ./tmp-share/key_pub
 cat ./tmp-share/key_pub >> ./tmp-share/id_rsa.pub
-docker cp dn03:/root/.ssh/id_rsa.pub ./tmp-share/key_pub
-cat ./tmp-share/key_pub >> ./tmp-share/id_rsa.pub
+# docker cp dn03:/root/.ssh/id_rsa.pub ./tmp-share/key_pub
+# cat ./tmp-share/key_pub >> ./tmp-share/id_rsa.pub
 docker cp spark-master:/root/.ssh/id_rsa.pub ./tmp-share/key_pub
 cat ./tmp-share/key_pub >> ./tmp-share/id_rsa.pub
 
 docker cp ./tmp-share/id_rsa.pub namenode:/root/.ssh/authorized_keys 
 docker cp ./tmp-share/id_rsa.pub dn01:/root/.ssh/authorized_keys
 docker cp ./tmp-share/id_rsa.pub dn02:/root/.ssh/authorized_keys
-docker cp ./tmp-share/id_rsa.pub dn03:/root/.ssh/authorized_keys
+# docker cp ./tmp-share/id_rsa.pub dn03:/root/.ssh/authorized_keys
 docker cp ./tmp-share/id_rsa.pub spark-master:/root/.ssh/authorized_keys
 docker exec -u root -it namenode chmod 0600 /root/.ssh/authorized_keys 
 docker exec -u root -it dn01 chmod 0600 /root/.ssh/authorized_keys
 docker exec -u root -it dn02 chmod 0600 /root/.ssh/authorized_keys
-docker exec -u root -it dn03 chmod 0600 /root/.ssh/authorized_keys
+# docker exec -u root -it dn03 chmod 0600 /root/.ssh/authorized_keys
 docker exec -u root -it spark-master chmod 0600 /root/.ssh/authorized_keys
 echo "==> Start ssh daemon in workers."
 docker exec -u root -it namenode /etc/init.d/ssh stop;
@@ -33,7 +33,7 @@ docker exec -u root -it dn01 /etc/init.d/ssh stop;
 docker exec -u root -it dn01 /etc/init.d/ssh start;
 docker exec -u root -it dn02 /etc/init.d/ssh stop;
 docker exec -u root -it dn02 /etc/init.d/ssh start;
-docker exec -u root -it dn03 /etc/init.d/ssh stop;
-docker exec -u root -it dn03 /etc/init.d/ssh start;
+# docker exec -u root -it dn03 /etc/init.d/ssh stop;
+# docker exec -u root -it dn03 /etc/init.d/ssh start;
 docker exec -u root -it spark-master /etc/init.d/ssh stop;
 docker exec -u root -it spark-master /etc/init.d/ssh start;
