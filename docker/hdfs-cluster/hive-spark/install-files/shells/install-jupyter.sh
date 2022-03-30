a2c8728cdab2fd309bb1b4f49129eb9f1fd9e5b5
shwsun <shwsun@naver.com>
2022-03-30 15:17:22 +0900

    pythin 3.8

--- docker/hdfs-cluster/hive-spark/install-files/shells/install-jupyter.sh
+++ docker/hdfs-cluster/hive-spark/install-files/shells/install-jupyter.sh
@@ -1,6 +1,9 @@
 apt-get update
+apt-get install -y python3.8  
+update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 apt-get install -y python3-pip iputils-ping
 ln /usr/bin/pip3 /usr/bin/pip  
+pip install update pip 
 pip install jupyterlab
 pip install pyspark==3.2.1
 pip install findspark
